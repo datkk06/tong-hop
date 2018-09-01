27247e6aa5087141059c95642307fb49cfd85f76
datpt <thanhdatkma96@gmail.com>
2018-09-01 10:39:40 +0700

    update scripts disable IPv6

--- script/1.ELK/filebeat-u.sh
+++ script/1.ELK/filebeat-u.sh
@@ -3,6 +3,14 @@
 source config.cfg
 source function.sh
 
+########### Disable IPv6 ##############
+
+cat > /etc/sysctl.conf << EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
 ########### Install filebeat ##############
 
 echocolor "Download and Install"
